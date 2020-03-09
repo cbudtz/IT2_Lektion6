10c9410b91b58438fac9e4e554083d7f5211a0e6
chbu <chbu@dtu.dk>
2020-03-09 16:02:55 +0100

    Threading Example!

---
+++ src/Thredder.java
@@ -0,0 +1,17 @@
+public class Thredder {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println("Thread " + Thread.currentThread().getName() + " is done");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).run();
+        }
+        System.out.println("Program ended!");
+    }
+}
