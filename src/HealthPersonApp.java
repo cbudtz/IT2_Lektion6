642f3daa55ce2f3bfd8e24b5d27c30ad7c8ccf1d
chbu <chbu@dtu.dk>
2020-03-09 13:45:00 +0100

    Polymorphism

---
+++ src/HealthPersonApp.java
@@ -0,0 +1,13 @@
+import javax.print.Doc;
+import java.util.List;
+
+public class HealthPersonApp {
+    List<HealthPersonnel> employees;
+
+    public static void main(String[] args) {
+        HealthPersonnel johnny = new HealthPersonnel();
+        Doctor doc = new Doctor();
+        HealthPersonnel rachel = new Nurse();
+
+    }
+}
